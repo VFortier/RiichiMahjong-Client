4f5e2081e8c36b978372c3ff206a60a13189aba6
Vincent Fortier <vincentf26@gmail.com>
2017-08-25 12:13:20 -0400

    Basic client/server communication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(private http: HttpClient) {}
+
+  title = 'Riichi Mahjong';
+  player: Player = {
+    name: ''
+  };
+
+  httpRes = '';
+
+  onFindAGame() {
+    // Make the HTTP request:
+    this.http.get('http://localhost:3600/').subscribe(data => {
+      // Read the result field from the JSON response.
+      console.log(data);
+      this.httpRes = data['success'];
+    });
+  }
+}
+
+export class Player {
+  name: string;
 }
